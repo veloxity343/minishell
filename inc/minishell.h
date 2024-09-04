ba3a23601881638c7b8cb1637d4083372e5aa235
Jaxz T <chengsoo99@gamil.com>
2024-09-04 21:18:48 +0800

    add readline header in quotatition

--- inc/minishell.h
+++ inc/minishell.h
@@ -10,8 +10,8 @@
 # include <errno.h>
 # include <fcntl.h>
 # include <limits.h>
-# include <readline/history.h>
-# include <readline/readline.h>
+# include "readline/history.h"
+# include "readline/readline.h"
 # include <signal.h>
 # include <stdbool.h>
 # include <stdio.h>
