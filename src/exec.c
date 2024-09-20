b1a02e1ebfeb37a2e8ee0628ca5ae34a5a65fa39
Jaxz T <chengsoo99@gamil.com>
2024-09-21 00:11:24 +0800

    add

--- src/exec.c
+++ src/exec.c
@@ -3,16 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 09:44:21 by chtan             #+#    #+#             */
-/*   Updated: 2024/09/15 14:40:24 by chtan            ###   ########.fr       */
+/*   Updated: 2024/09/16 17:18:29 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 #include "../inc/exec.h"
 #include "../inc/define_lib.h"
+// this is the struct of token part
+/*
+ttypedef enum e_token_type
+{
+	TOKEN_WORD,            // General word or command
+	TOKEN_PIPE,            // Pipe (|)
+	TOKEN_REDIRECT_IN,     // Input redirection (<)
+	TOKEN_REDIRECT_OUT,    // Output redirection (>)
+	TOKEN_REDIRECT_APPEND, // Output redirection append (>>)
+	TOKEN_HEREDOC,         // Here document (<<)
+	TOKEN_QUOTE_SINGLE,    // Single quote (')
+	TOKEN_QUOTE_DOUBLE,    // Double quote (")
+	TOKEN_ENV_VAR,         // Environment variable ($)
+	TOKEN_END              // End of tokens
+}	t_token_type;
+
+// Struct for a token
+typedef struct s_token
+{
+	t_token_type type; // Type of token
+	char *value;       // Value of the token (e.g., the command or file name)
+}	t_token;
+
+*/
 /*
 	requirement for execute
 	1) token type
@@ -23,9 +47,14 @@
 this is the main function of exec part
 15/9) cause i can't comfirm how the token gonna pass to me but now i know
 and also figure out the simple exec() function
-
 */
-int exec_main(t_token *tokens)
+
+static int ft_check()
 {
 	;
+}
+
+int exec_main(t_token *tokens)
+{
+	ft_check(tokens->type);// function not complete
 }
--- src/signal.c
+++ src/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 12:33:58 by chtan             #+#    #+#             */
-/*   Updated: 2024/09/13 12:28:59 by chtan            ###   ########.fr       */
+/*   Updated: 2024/09/16 15:00:33 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	exit_sig(int sig)
 	if (EXIT_SUCCESS == sig)
 	{
 		ft_printf("exit\n");
-≈		exit(0);
+		exit(0);
 	}
 	if (sig == EXIT_FAILURE)
 	{
