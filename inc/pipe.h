1936324f75cd41d37f796aeb218b4c7c4fc9a25e
Jaxz T <chengsoo99@gamil.com>
2024-10-13 11:25:18 +0800

    comment the undifined header

--- inc/pipe.h
+++ inc/pipe.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 13:17:45 by chtan             #+#    #+#             */
-/*   Updated: 2024/09/13 15:29:57 by chtan            ###   ########.fr       */
+/*   Updated: 2024/10/13 11:24:41 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPE_H
 # define PIPE_H
 # include "minishell.h"
-# include <_pid_t.h>
+// # include <_pid_t.h>
 
 //pipex
 void	pipe_main(char **av, int ac, char **env);
