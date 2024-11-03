0ee0dede45e15ccfed985ef73bb4dee966d52f55
Cheng Soo Tan <chtan@u90z06s02.42kl.edu.my>
2024-11-03 11:16:40 +0800

    add a ~ before priting pwd

--- src/runtime/builtins/cd.c
+++ src/runtime/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 18:09:48 by rcheong           #+#    #+#             */
-/*   Updated: 2024/11/02 16:36:04 by chtan            ###   ########.fr       */
+/*   Updated: 2024/11/03 10:57:21 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ static int ft_go_oldpwd(t_mini *mini)
 	sig = chdir(oldpwd);
 	if (sig == -1)
 		return (ft_putstr_fd(strerror(errno), 2), 1);
+	ft_putchar_fd('~', 1);
 	ft_putstr_fd(oldpwd, 1);
 	ft_putchar_fd('\n', 1);
 	ft_update_env(mini, "PWD", oldpwd, false);
